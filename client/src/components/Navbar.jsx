68ccdaf958d5967304a36a57f495b19d6672aeb4
Akshay0805 <akshaymm0805@gmail.com>
2026-04-08 23:10:19 +0530

    card

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { assets } from '../assets/assets'
-import { MenuIcon, SearchIcon, XIcon } from 'lucide-react'
+import { MenuIcon, SearchIcon, TicketIcon, TicketPlus, XIcon } from 'lucide-react'
 import { useClerk, useUser , UserButton } from '@clerk/react'
 
 const Navbar = () => {
     const [isOpen , setIsOpen] = useState(false)
     const {user} = useUser()
     const {openSignIn} = useClerk()
+
+    const navigate = useNavigate()
 
   return (
     <div className='fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5'>
@@ -21,9 +23,9 @@ const Navbar = () => {
         ${isOpen ? 'max-md:w-full' : 'max-md:w-0'}`}>
            
             <XIcon className='md:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer' onClick={()=>setIsOpen(!isOpen)}/>
-            <Link onClick={()=>{scrollTo(0,0), setIsOpen(false)}} to='/'>Home</Link>
-            <Link onClick={()=>{scrollTo(0,0), setIsOpen(false)}} to='/movies'>Movies</Link>
-            <Link onClick={()=>{scrollTo(0,0), setIsOpen(false)}}to='/favorite'>Favorites</Link>
+            <Link onClick={()=>{scrollTo(0,0); setIsOpen(false)}} to='/'>Home</Link>
+            <Link onClick={()=>{scrollTo(0,0); setIsOpen(false)}} to='/movies'>Movies</Link>
+            <Link onClick={()=>{scrollTo(0,0); setIsOpen(false)}} to='/favorite'>Favorites</Link>
         </div>
 
         <div className='flex items-center gap-8'>
@@ -36,7 +38,11 @@ const Navbar = () => {
                         Login
                     </button>
                ) : (
-                    <UserButton></UserButton>
+                    <UserButton>
+                        <UserButton.MenuItems>
+                                <UserButton.Action label="My Booking" labelIcon={<TicketPlus width={15}/>} onClick={()=>navigate('/my-bookings')}/>
+                        </UserButton.MenuItems>
+                    </UserButton>
                )
             }
             
--- client/src/pages/SeatLayout.jsx
+++ client/src/pages/SeatLayout.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-
 const SeatLayout = () => {
   return (
-    <div>SeatLayout</div>
+    <div>
+      
+    </div>
   )
 }
 
