881d7e99e9963e2067e72728c3b3d3f7a1964012
Akshay0805 <akshaymm0805@gmail.com>
2026-04-08 11:56:10 +0530

    change1

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
                         Login
                     </button>
                ) : (
-                    <UserButton />
+                    <UserButton></UserButton>
                )
             }
             
