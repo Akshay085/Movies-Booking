0b3b8ece18dbaa78fe09a0ae9a721e13c26373d0
Akshay0805 <akshaymm0805@gmail.com>
2026-04-08 11:28:29 +0530

    navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
         ${isOpen ? 'max-md:w-full' : 'max-md:w-0'}`}>
            
             <XIcon className='md:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer' onClick={()=>setIsOpen(!isOpen)}/>
-            <Link onClick={()=>{scrollTo(0,0), setIsOpen(false)}} to='/'>Horme</Link>
+            <Link onClick={()=>{scrollTo(0,0), setIsOpen(false)}} to='/'>Home</Link>
             <Link onClick={()=>{scrollTo(0,0), setIsOpen(false)}} to='/movies'>Movies</Link>
             <Link onClick={()=>{scrollTo(0,0), setIsOpen(false)}}to='/favorite'>Favorites</Link>
         </div>
