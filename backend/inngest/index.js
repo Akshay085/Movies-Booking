ad1c735064ce256ced8d9322af12272e6be1d88b
Akshay0805 <akshaymm0805@gmail.com>
2026-04-23 01:49:32 +0530

    index

--- backend/inngest/index.js
+++ backend/inngest/index.js
@@ -1,5 +1,6 @@
 import { Inngest } from "inngest";
 import User from "../models/userModel.js";
+import { connectDB } from "../configs/db.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "movie-ticket-booking" });
@@ -7,12 +8,15 @@ export const inngest = new Inngest({ id: "movie-ticket-booking" });
 const syncUserCreation = inngest.createFunction(
     { id: 'sync-user-from-clerk', event: 'clerk/user.created' }, 
     async ({ event }) => {
-        const {id , first_name, last_name, email_addresses, image_url} = event.data
+        await connectDB();
+        console.log("Webhook Received:", event.data);
+        const payload = event.data.data || event.data;
+        const { id, first_name, last_name, email_addresses, image_url } = payload;
         const userData = {
             _id: id,
-            email: email_addresses[0].email_address,
-            name: first_name + ' ' + last_name,
-            image: image_url
+            email: email_addresses && email_addresses.length > 0 ? email_addresses[0].email_address : "",
+            name: first_name + (last_name ? ' ' + last_name : ''),
+            image: image_url || ""
         }
         await User.create(userData)
     }
@@ -23,7 +27,10 @@ const syncUserCreation = inngest.createFunction(
 const syncUserDeletion = inngest.createFunction(
     { id: 'delete-user-with-clerk', event: 'clerk/user.deleted' }, 
     async ({ event }) => {
-        const {id} = event.data
+        await connectDB();
+        console.log("Delete Webhook Received:", event.data);
+        const payload = event.data.data || event.data;
+        const { id } = payload;
         await User.findByIdAndDelete(id)
     }
 )
@@ -33,12 +40,15 @@ const syncUserDeletion = inngest.createFunction(
 const syncUserUpdation = inngest.createFunction(
     { id: 'update-user-from-clerk', event: 'clerk/user.updated' }, 
     async ({ event }) => {
-        const {id , first_name, last_name, email_addresses, image_url} = event.data
+        await connectDB();
+        console.log("Update Webhook Received:", event.data);
+        const payload = event.data.data || event.data;
+        const { id, first_name, last_name, email_addresses, image_url } = payload;
         const userData = {
             _id: id,
-            email: email_addresses[0].email_address,
-            name: first_name + ' ' + last_name,
-            image: image_url
+            email: email_addresses && email_addresses.length > 0 ? email_addresses[0].email_address : "",
+            name: first_name + (last_name ? ' ' + last_name : ''),
+            image: image_url || ""
         }
         await User.findByIdAndUpdate(id, userData)
     }
